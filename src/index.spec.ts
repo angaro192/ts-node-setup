ae52f462842783ca3537ea218cd011012137adf5
Augusto Almeida <augustalmeid@gmail.com>
2023-01-01 15:30:12 -0300

    removido arquivo de teste, para configurar depois

--- src/index.spec.ts
+++
@@ -1,5 +0,0 @@
-import Person from './index';
-it('should sum', () => {
-  const person = new Person();
-  expect(person.sayMayName()).toBe('Augusto');
-});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-class Person {
-  sayMayName() {
-    return 'Augusto';
-  }
-}
-export default Person;
+import * as express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.get('', (req: Request, res: Response) => {
+  res.send('Hello Word');
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
